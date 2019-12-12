ea3a1a770b7c40981a92f2a73a7b849052a5215f
Sosunokz7 <vits205070@gmail.com>
2019-12-13 00:41:28 +0700

    SorBukv

---
+++ Python/SortBuk.py
@@ -0,0 +1,25 @@
+Buk=['A','B','C','D','E','F','G','H','I']
+
+slow=list('ADDSACABD')
+
+def ShBuk(Buk,slow):
+    mas=[]
+    for i in slow:
+        for i1 in range(len(Buk)):
+            if i==Buk[i1]:
+                mas.append(i1)
+    mas= Sor(mas)
+    for m in range(len(mas)):
+        for en,b in enumerate(Buk):
+            if mas[m]==en:
+                mas[m]=b
+    return mas
+def Sor(mas):
+    x=len(mas)-1
+    for i in range(x):
+        for i1 in range(x-i):
+            if mas[i1]>mas[i1+1]:
+                mas[i1],mas[i1+1]=mas[i1+1],mas[i1]
+    return mas
+x=ShBuk(Buk,slow)
+print(str(x))
